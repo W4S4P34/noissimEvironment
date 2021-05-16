8d4c31082d0d3e10647935c60a976b710549b397
Where is Neo? <alec.bui.vhp@gmail.com>
2021-05-16 20:05:38 +0700

    (+) Progress saver + Level calculator

---
+++ Assets/ScriptableObjects/CharacterLevel/CharacterLevel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterLevel : ScriptableObject
+{
+    [SerializeField]
+    private long[] expStage = { 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000 };
+
+    public double calculateLevel()
+    {
+        long currentExp = ProgressSerial.getInstance().ExpToSave;
+
+        Debug.Log(currentExp);
+
+        double level = 1;
+
+        for(int i = 0; i < expStage.Length; i++)
+        {
+            if(currentExp > expStage[i])
+            {
+                currentExp -= expStage[i];
+                level++;
+            }
+            else
+            {
+                level += currentExp * 1.0 / expStage[i];
+                break;
+            }
+        }
+
+        return level;
+    }
+}
---
+++ Assets/Scripts/ProgressDataSaver/ProgressSerial.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+static class Constants
+{
+    public const String fileName = "/ProgressData.dat";
+}
+
+public class ProgressSerial
+{
+    private long rubyToSave;
+
+    public long RubyToSave
+    { get; set; }
+
+    private long expToSave;
+
+    public long ExpToSave
+    { get; set; }
+
+    public static ProgressSerial getInstance()
+    {
+        return SingletonHelper.INSTANCE;
+    }
+
+    private static class SingletonHelper
+    {
+        public static readonly ProgressSerial INSTANCE = new ProgressSerial();
+    }
+
+
+    public void saveData()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + Constants.fileName);
+        ProgressData data = new ProgressData();
+        data.savedRuby = rubyToSave;
+        data.savedExp = expToSave;
+        bf.Serialize(file, data);
+        file.Close();
+        Debug.Log("Game data saved!");
+    }
+
+    public void loadData()
+    {
+        if (File.Exists(Application.persistentDataPath + Constants.fileName))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + Constants.fileName, FileMode.Open);
+            ProgressData data = (ProgressData)bf.Deserialize(file);
+            file.Close();
+            rubyToSave = data.savedRuby;
+            expToSave = data.savedExp;
+            Debug.Log("Game data loaded!");
+        }
+        else
+            Debug.LogError("There is no save data!");
+    }
+
+}
+
+[Serializable]
+class ProgressData
+{
+    public long savedRuby;
+    public long savedExp;
+}
