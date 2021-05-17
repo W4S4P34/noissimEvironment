67f41f315c13501d03953c0606dcf602a550006c
Where is Neo? <alec.bui.vhp@gmail.com>
2021-05-17 16:20:31 +0700

    (+) Start menu

---
+++ Assets/Scripts/MenuUI/SceneSwitcher.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    [SerializeField]
+    GameObject exitPanel;
+
+    public void continueGame()
+    {
+        if (ProgressSerial.getInstance().checkExist())
+        {
+            ProgressSerial.getInstance().loadData();
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        else
+        {
+            restartGame();
+        }
+    }
+
+    public void restartGame()
+    {
+        ProgressSerial.getInstance().restartData();
+        ProgressSerial.getInstance().saveData();
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void showDialog()
+    {
+        if (exitPanel)
+        {
+            exitPanel.SetActive(true);
+        }
+    }
+
+    public void onUserClickYesNo(int choice)
+    {
+        if(choice == 1)
+        {
+            ProgressSerial.getInstance().saveData();
+            Application.Quit();
+        }
+        else
+        {
+            exitPanel.SetActive(false);
+        }
+    }
+}
