c2f3b80b3d1aa8315f324ebba2a6df3bd551b257
La Thành Thái <lathanhthai1905@gmail.com>
2021-05-18 20:20:42 +0700

    Add minimap to dungeon and combat scene
    Co-authored-by: Hiếu Phụng <alec.bui.vhp@gmail.com>
    Minimap!!!

---
+++ Assets/Scripts/MapGenerator/DungeonRoomMinimapActivators.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonRoomMinimapActivators : MonoBehaviour
+{
+    [SerializeField] private GameObject room;
+    private DungeonRoomBuilder roomBuilder;
+
+    // Event
+    [SerializeField] private GameEvent OnUpdateMinimap;
+
+    private const int _ROOM_MINIMAP_WIDTH_ = 1; // == _ROOM_MINIMAP_HEIGHT_
+    // private const int _ROOM_MINIMAP_HEIGHT_ = 1;
+
+    private void Awake()
+    {
+        roomBuilder = room.GetComponent<DungeonRoomBuilder>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer != LayerMask.NameToLayer("Player")) return;
+
+        GameObject[,] minimapRealization = DungeonMinimapBuilder.GetMapRealization();
+        int x = (int)roomBuilder.Position.x, y = (int)roomBuilder.Position.y;
+
+        DungeonMinimapRoomBuilder minimapRoomBuilder =
+            minimapRealization[x, y].gameObject.GetComponent<DungeonMinimapRoomBuilder>();
+        minimapRoomBuilder.IsVisited = true;
+        minimapRoomBuilder.IsCurrentlyVisited = true;
+
+        for (int i = 0, neighbours = 4; i < neighbours; i++)
+        {
+            if (minimapRoomBuilder.Neighbours[i] == null)
+            {
+                continue;
+            }
+
+            DungeonMinimapRoomBuilder neighbourMinimapRoomBuilder =
+                minimapRoomBuilder.Neighbours[i].GetComponent<DungeonMinimapRoomBuilder>();
+            neighbourMinimapRoomBuilder.IsCurrentlyVisited = false;
+        }
+
+        DungeonMinimapBuilder.Camera.transform.position =
+            minimapRealization[x, y].gameObject.transform.position + new Vector3(0, 0, -10);
+
+        OnUpdateMinimap.Raise();
+    }
+}
